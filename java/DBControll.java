0db23c9e3170d362576a54328157a754c98046e8
5544451 <62584670+5544451@users.noreply.github.com>
2022-12-12 22:31:03 +0900

    Update DBControll.java
    데베 제주도에서 웹프로 필요한거 쿼리값바꿈

--- java/DBControll.java
+++ java/DBControll.java
@@ -18,7 +18,7 @@ public class DBControll {
 	    public DBControll() {
 	    	USERNAME = "root";
 	    	PASSWORD = "root";
-	    	URL = "jdbc:mysql://localhost:3306/jeju";
+	    	URL = "jdbc:mysql://localhost:3306/eco";
 	    }
 	    
 	    private void connect() {
@@ -39,11 +39,11 @@ public class DBControll {
 	        }
 	    }
 	    
-	    //db에 데이터 삽입하는 메서드
-	    public void insertDB(String[] mem, String table){
+	    //db에 멤버 데이터 삽입하는 메서드
+	    public boolean insertMember(String[] mem){
 	        //쿼리문 준비
 	    	connect();
-	        String sql = String.format("insert into %s values(?,?,?)", table);
+	        String sql = String.format("insert into member values(?,?,?)");
 	        
 	        PreparedStatement pstmt = null;
 	        try {
@@ -55,6 +55,7 @@ public class DBControll {
 	            if(result==1) {
 	                System.out.println("data insert success!");
 	                conn.close();
+	                return true;
 	            } 
 	        } catch (Exception e) {
 	            System.out.println("data insert fail!"+ e);
@@ -65,6 +66,7 @@ public class DBControll {
 	                }
 	            } catch (Exception e2) {}
 	        }
+			return false;
 	    }
 	    
 	    //1행만 가져오는 메서드
@@ -80,8 +82,8 @@ public class DBControll {
 	            ResultSet rs = pstmt.executeQuery();
 	            if(rs.next()) {
 	            	info[0] = rs.getString("email");
-	            	info[1] = rs.getString("pswd");
-	            	info[2] = rs.getString("list");  
+	            	info[1] = rs.getString("nickname"); 
+	            	info[2] = rs.getString("pswd");
 	            }
 	        } catch (Exception e) {
 	            System.out.println("select 메서드 예외발생" + e);
@@ -97,59 +99,53 @@ public class DBControll {
 	    }
 	    
 	    //데베 전체가져옴
-		public List<Place> selectTable(String table) {
-			connect();
-			String sql ="";
-			if(table =="tourist_destination" || table =="lodgment")
-				sql = String.format("select * from %s order by 조회수 desc", table);
-			else
-				sql = String.format("select * from %s", table);
-	        PreparedStatement pstmt = null;
-	        List<Place> info = new ArrayList<Place>();
-	        System.out.print(sql);
-	        try {
-	            pstmt = conn.prepareStatement(sql);
-	            ResultSet rs = pstmt.executeQuery();
-	            while (rs.next()) {
-	            	Place P = new Place();
-					
-					P.type = rs.getString("콘텐츠분류"); 
-					P.view = rs.getInt("조회수");
-	            	P.name = rs.getString("제목");
-	            	P.roadaddress = rs.getString("지번주소");
-	            	P.lotaddress = rs.getString("도로명주소");
-	            	P.latitude = Double.parseDouble(rs.getString("위도"));
-	            	P.longitude = Double.parseDouble(rs.getString("위도"));
-	            	info.add(P);
-	            }
-	            pstmt.close();
-	        } catch (Exception e) {
-	            System.out.println("select method error : " + e);
-	        }    finally {
-	            try {
-	                if(pstmt!=null && !pstmt.isClosed()) {
-	                    conn.close();
-	                }
-	            } catch (Exception e2) {}
-	        }
-	        return info;
-	    }
+//		public List<Place> selectTable(String table) {
+//			connect();
+//			String sql ="";
+//			if(table =="tourist_destination" || table =="lodgment")
+//				sql = String.format("select * from %s order by 조회수 desc", table);
+//			else
+//				sql = String.format("select * from %s", table);
+//	        PreparedStatement pstmt = null;
+//	        List<Place> info = new ArrayList<Place>();
+//	        System.out.print(sql);
+//	        try {
+//	            pstmt = conn.prepareStatement(sql);
+//	            ResultSet rs = pstmt.executeQuery();
+//	            while (rs.next()) {
+//	            	Place P = new Place();
+//					
+//					P.type = rs.getString("콘텐츠분류"); 
+//					P.view = rs.getInt("조회수");
+//	            	P.name = rs.getString("제목");
+//	            	P.roadaddress = rs.getString("지번주소");
+//	            	P.lotaddress = rs.getString("도로명주소");
+//	            	P.latitude = Double.parseDouble(rs.getString("위도"));
+//	            	P.longitude = Double.parseDouble(rs.getString("경도"));
+//	            	info.add(P);
+//	            }
+//	            pstmt.close();
+//	        } catch (Exception e) {
+//	            System.out.println("select method error : " + e);
+//	        }    finally {
+//	            try {
+//	                if(pstmt!=null && !pstmt.isClosed()) {
+//	                    conn.close();
+//	                }
+//	            } catch (Exception e2) {}
+//	        }
+//	        return info;
+//	    }
 		
-		
-	  
 		//조건에 맞는 행을 DB에서 수정(갱신)    하는 메서드
 	    public void updateDB(String id) {
 	    	connect();
-	        String sql = "update member set name =?, password =?, email =?, list =? where id=?";
+	        String sql = "update member set nickname =?, password =?, where email=?";
 	        PreparedStatement pstmt = null;
 	        try {
 	            pstmt = conn.prepareStatement(sql);
-	            pstmt.setString(1,"nickname");
-	            pstmt.setString(2,id);
+	            pstmt.setString(2,"nickname");
 	            pstmt.setString(3,"pswd");
-	            pstmt.setString(4,"email");
-	            pstmt.setString(5,"list");
-	            pstmt.setString(6,"sendmail");
 	            pstmt.executeUpdate();
 	        } catch (Exception e) {
 	            System.out.println("update 예외 발생" + e);
